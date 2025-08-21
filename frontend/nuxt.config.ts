cb23bffdfc6daf59765154ec65ae650e43dd3642
Nattawut-007 <67319010007@loeitech.ac.th>
2025-08-21 13:40:42 +0700

    update vite-url

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   /* app: {
-    baseURL: '/s07/'   
+    baseURL: '/s07/'   5555555
   }, */
   vite: {
     plugins: [tailwindcss()],
